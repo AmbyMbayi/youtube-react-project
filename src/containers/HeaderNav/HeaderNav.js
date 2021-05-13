24b9481963527ccbda66ca6b5b23eaddb20a4b1e
AmbyMbayi <ambymbayi54@gmail.com>
2021-05-13 10:51:53 +0300

    navbar created

--- src/containers/HeaderNav/HeaderNav.js
+++ src/containers/HeaderNav/HeaderNav.js
@@ -6,11 +6,26 @@ import logo from '../../assets/images/logo.png';
 export class HeaderNav extends Component {
 	render() {
 		return (
-			<div>
-				<img src={logo} alt="" />
-				<form action="">
-					<input type="text" placeholder="search" />
+			<div className="navbar__header">
+				<div className="image__logo">
+					<img src={logo} alt="" />
+					<h2>TealTube</h2>
+				</div>
+
+				<form action="" className="input__form">
+					<div className="search__input">
+						<input type="text" placeholder="search" />
+					</div>
 				</form>
+				<div className="microphone">
+					<i className="bx bxs-microphone " />
+				</div>
+				<div className="right__icons">
+					<i className="bx bxs-camera-movie" />
+					<i className="bx bxs-grid" />
+					<i className="bx bxs-bell-ring" />
+					<i className="bx bxs-user" />
+				</div>
 			</div>
 		);
 	}
