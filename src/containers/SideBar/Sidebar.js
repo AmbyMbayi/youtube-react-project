c60feddcaed6c5619c63afc2868c496ed9ce7de6
AmbyMbayi <ambymbayi54@gmail.com>
2021-05-13 14:09:25 +0300

    sidebar created

--- src/containers/SideBar/Sidebar.js
+++ src/containers/SideBar/Sidebar.js
@@ -7,11 +7,17 @@ export class Sidebar extends Component {
 		return (
 			<div className="sidebar">
 				<SidebarItem label="Home" style="bx bxs-home" />
-				<SidebarItem label="Explore" style="bx bxs-compass" />
-				<SidebarItem label="Subscriptions" style="bx bx-tv" />
-				<SidebarItem label="Library" style="bx bxs-videos" />
+				<SidebarItem label="Trending" style="bx bxs-flame" />
+				<SidebarItem label="Followers" style="bx bx-tv" />
+
 				<SidebarItem label="History" style="bx bx-history" />
 				<SidebarItem label="Watch Later" style="bx bxs-time-five" />
+				<SidebarItem label="Like Videos" style="bx bxs-videos" />
+
+				<SidebarItem label="Movies and Shows" style="bx bx-movie" />
+				<SidebarItem label="Report History" style="bx bxs-flag-alt" />
+				<SidebarItem label="Help" style="bx bxs-help-circle" />
+				<SidebarItem label="Send Feedback" style="bx bxs-chat" />
 			</div>
 		);
 	}
