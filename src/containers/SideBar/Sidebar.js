e62c84b2d84242a3d55a4319bdc96a8c85356f61
AmbyMbayi <ambymbayi54@gmail.com>
2021-05-13 15:01:47 +0300

    sidebar done

--- src/containers/SideBar/Sidebar.js
+++ src/containers/SideBar/Sidebar.js
@@ -6,18 +6,21 @@ export class Sidebar extends Component {
 	render() {
 		return (
 			<div className="sidebar">
+				<div className="divider" />
 				<SidebarItem label="Home" style="bx bxs-home" />
 				<SidebarItem label="Trending" style="bx bxs-flame" />
 				<SidebarItem label="Followers" style="bx bx-tv" />
-
+				<div className="divider" />
 				<SidebarItem label="History" style="bx bx-history" />
 				<SidebarItem label="Watch Later" style="bx bxs-time-five" />
-				<SidebarItem label="Like Videos" style="bx bxs-videos" />
-
+				<SidebarItem label="Like Videos" style="bx bxs-like" />
+				<div className="divider" />
 				<SidebarItem label="Movies and Shows" style="bx bx-movie" />
+				<div className="divider" />
 				<SidebarItem label="Report History" style="bx bxs-flag-alt" />
 				<SidebarItem label="Help" style="bx bxs-help-circle" />
 				<SidebarItem label="Send Feedback" style="bx bxs-chat" />
+				<div className="divider" />
 			</div>
 		);
 	}
